5792b4f678b8136a15074cdbf10342d827b9c555
Bill Fero <billfero@gmail.com>
2015-12-10 17:18:31 +0000

    Added drop shadow to LocationDetail title.

--- app/src/main/java/com/oandmdigital/mappingapp/ui/LocationDetailActivity.java
+++ app/src/main/java/com/oandmdigital/mappingapp/ui/LocationDetailActivity.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.oandmdigital.mappingapp.R;
 import com.oandmdigital.mappingapp.model.Shop;
@@ -19,7 +20,7 @@ import com.oandmdigital.mappingapp.model.ShopData;
 /**
  * References
  * [1] http://stackoverflow.com/questions/31662416/show-collapsingtoolbarlayout-title-only-when-collapsed
- *
+ * [2] http://blog.grafixartist.com/add-a-toolbar-elevation-on-pre-lollipop/
  */
 
 public class LocationDetailActivity extends AppCompatActivity {
@@ -45,11 +46,39 @@ public class LocationDetailActivity extends AppCompatActivity {
         toolbarBackdrop.setImageResource(ShopData.getImageDrawable(position));
 
 
-        // TODO load the shop details
+        // load the shop details
+        // content title
         final TextView locationTitle = (TextView) findViewById(R.id.location_title);
         final TextView locationDistance = (TextView) findViewById(R.id.location_distance);
+
         locationTitle.setText(location.getName());
         locationDistance.setText(String.format("%s %s", String.valueOf(location.getDistance()), "mi"));
+
+        // content detail
+        TextView locationAddress = (TextView) findViewById(R.id.location_address);
+        TextView locationPostalCode = (TextView) findViewById(R.id.location_postal_code);
+        TextView locationTelephone = (TextView) findViewById(R.id.location_telephone);
+        TextView locationUrl = (TextView) findViewById(R.id.location_url);
+        TextView locationOpeningTimes = (TextView) findViewById(R.id.location_opening_times);
+        TextView locationRating = (TextView) findViewById(R.id.location_rating);
+
+        locationAddress.setText(String.format("%s, %s",
+                location.getAddress().getStreet(), location.getAddress().getArea()));
+        locationPostalCode.setText(location.getAddress().getPostalCode());
+        locationTelephone.setText(location.getTelephone());
+        locationUrl.setText(location.getUrl());
+        locationRating.setText(String.format("Rating: %.1f", location.getRating()));
+
+
+        // TODO open drop down displaying opening times
+        locationOpeningTimes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LocationDetailActivity.this, "Display opening times", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // TODO display star ratings
 
 
         // TODO launch Google Navigation from the FAB
